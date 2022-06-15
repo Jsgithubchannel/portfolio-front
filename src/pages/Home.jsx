4bf0995986d42d9a5265a6b548ef3f24a3c4b5cc
Jsgithubchannel <jsschannel99@gmail.com>
2022-06-16 01:07:06 +0900

    Feat(Home): Add element & style

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,47 @@
 import React from "react";
-
+import styles from "./Home.module.scss";
 const Home = () => {
-  return <div></div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.paragraph}>
+        <h1>
+          Jisu Hong,
+          <br />
+          Front-End developer
+        </h1>
+        <div className={styles.underline} />
+        <p>
+          Hello! 👋 My name is Jisu Hong <br />
+          and I’m a front-end developer based in South Korea.
+          <br />I values cooperation and communication.
+        </p>
+      </div>
+      <img src="assets/real_me.jpeg" alt="my pic" />
+      <div className={styles.links}>
+        <a
+          href="https://github.com/Jsgithubchannel"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github
+        </a>
+        <a
+          href="https://breathtaking-life.tistory.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Blog
+        </a>
+        <a
+          href="https://www.linkedin.com/in/jisu-hong-7a9683226/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LinkedIn
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
