df4e834a9e9b945038e21279c0eeb7cbc8753223
Jsgithubchannel <jsschannel99@gmail.com>
2022-06-15 23:40:12 +0900

    Feat: Add navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import styles from "./Navbar.module.scss";
+import useWindowDimensions from "../hooks/useWindowDimensions";
 
 const Navbar = () => {
-  return <div></div>;
+  const [showMenu, setShowMenu] = useState(false);
+  const { width } = useWindowDimensions();
+
+  useEffect(() => {
+    width > 1024 ? setShowMenu(true) : setShowMenu(false);
+  }, [width]);
+  return (
+    <div className={styles.container}>
+      <span className={styles.logo}>Jisu Hong</span>
+      {showMenu ? (
+        <div className={styles.menu}>
+          <ul>HOME</ul>
+          <ul>ABOUT</ul>
+          <ul>PROJECT</ul>
+          <ul>CONTACT</ul>
+        </div>
+      ) : (
+        <FontAwesomeIcon
+          icon={faBars}
+          className={styles.barIcon}
+          // onClick={() => {
+          //   setShowDropDownMenu(true);
+          // }}
+        />
+      )}
+    </div>
+  );
 };
 
 export default Navbar;
