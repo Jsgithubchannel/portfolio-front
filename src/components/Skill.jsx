dcf3e0d6b54317b75ca87264437a7b2d9602381c
Jsgithubchannel <jsschannel99@gmail.com>
2022-06-16 21:24:17 +0900

    Feat(About): Add skillset

---
+++ src/components/Skill.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import styles from "./Skill.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHtml5,
+  faPython,
+  faGitAlt,
+} from "@fortawesome/free-brands-svg-icons";
+
+const icons = {
+  "Front-End": faHtml5,
+  "Back-End": faPython,
+  Tool: faGitAlt,
+};
+
+const Skill = ({ skill }) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.circle}>
+        <FontAwesomeIcon icon={icons[skill.title]} className={styles.icon} />
+      </div>
+      <h1>{skill.title}</h1>
+      {Object.entries(skill.stack).map(([key, val]) => (
+        <ul key={key} class>
+          {val}
+        </ul>
+      ))}
+    </div>
+  );
+};
+
+export default Skill;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import Skill from "../components/Skill";
+import styles from "./About.module.scss";
+import skills from "../static/skills.json";
 
 const About = () => {
-  return <div></div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}>
+        <h1>About Me</h1>
+        <div className={styles.underline} />
+      </div>
+      {skills.map((skill) => (
+        <Skill skill={skill} key={skill.id} />
+      ))}
+    </div>
+  );
 };
 
 export default About;
