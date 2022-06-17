f44f36f3e3025188bae03c4da6ea08d0dd31665b
Jsgithubchannel <jsschannel99@gmail.com>
2022-06-17 23:37:35 +0900

    Feat(Project): Load projects from server

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,20 +1,32 @@
-import React, { useEffect } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import styles from "./Project.module.scss";
 import Title from "../components/Title";
 import { getProject } from "../services/projectService";
 
 const Project = () => {
-  useEffect(() => {
-    const get = async () => {
-      getProject().then((response) => {
-        console.log(response);
-      });
-    };
-    get();
+  const [projects, setProjects] = useState([]);
+  const loadProjectsFromServer = useCallback(async () => {
+    getProject().then((response) => {
+      setProjects(response);
+    });
   }, []);
+  useEffect(() => {
+    loadProjectsFromServer();
+  }, [loadProjectsFromServer]);
   return (
     <div className={styles.container}>
       <Title title={"Project"} />
+      <div className={styles.wrapper}>
+        {projects.map((project) => {
+          return (
+            <img
+              src={`http://localhost:8000${project.photo}`}
+              alt="project_screenshot"
+              key={project.id}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
